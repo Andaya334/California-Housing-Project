673bf013984eb31d5aa3fc35529a850c31634f0d
Andaya334 <Andaya334@gmail.com>
2023-04-24 16:19:20 -0700

    new save

--- app.py
+++ app.py
@@ -1,52 +1,101 @@
 import numpy as np
 import re
 import datetime as dt
-
+import sqlite3
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
-from sqlalchemy.sql import exists  
+from sqlalchemy.sql import exists
+from flask import Flask, render_template, jsonify
 
-from flask import Flask, jsonify
-
-#Setup DB
-engine = create_engine("sqlite:///Resources/Housing_Table.sqlite")
-
-#use Base to reflect DB and reflect tables
-Base = automap_base()
-Base.prepare(engine, reflect=True)
-
-#set up flask
-print(__name__)
+# create a Flask app
 app = Flask(__name__)
 
-#List available APIs
-@app.route("/")
+# connect to the SQLite database
+conn = sqlite3.connect('Housing_Table.sqlite')
+cur = conn.cursor()
+
+# check that we can fetch data from the Housing table
+res = cur.execute('SELECT * FROM Housing')
+print(res.fetchone())
+
+# define some API endpoints
+@app.route('/')
 def home():
-    """List all available api routes."""
-    return (
-        f""
-    )
+    return render_template('index.html')
 
-@app.route("/api/v1.0/sql")
-def sql():
-    session = Session(engine)
-    results = session.query().all()
-    session.close
-    #convert list of tuples into normal list
-    #CODE HERE
+@app.route('/api/housing')
+def get_housing():
+    cur.execute('SELECT * FROM Housing')
+    data = cur.fetchall()
+    return jsonify(data)
 
-    return jsonify()
+@app.route('/api/housing/distance_to_la/<float:distance>')
+def get_housing_by_distance_to_la(distance):
+    cur.execute('SELECT * FROM Housing WHERE Distance_to_LA <= ?', (distance,))
+    data = cur.fetchall()
+    return jsonify(data)
 
+@app.route('/api/housing/distance_to_sf/<float:distance>')
+def get_housing_by_distance_to_sf(distance):
+    cur.execute('SELECT * FROM Housing WHERE Distance_to_SanFrancisco <= ?', (distance,))
+    data = cur.fetchall()
+    return jsonify(data)
 
+@app.route('/api/housing/distance_to_sd/<float:distance>')
+def get_housing_by_distance_to_sd(distance):
+    cur.execute('SELECT * FROM Housing WHERE Distance_to_SanDiego <= ?', (distance,))
+    data = cur.fetchall()
+    return jsonify(data)
 
+@app.route('/api/housing/distance_to_sj/<float:distance>')
+def get_housing_by_distance_to_sj(distance):
+    cur.execute('SELECT * FROM Housing WHERE Distance_to_SanJose <= ?', (distance,))
+    data = cur.fetchall()
+    return jsonify(data)
 
-#Creat a dictionary from the raw data and appemd to a list of sql database
-##CODE HERE!
+@app.route('/api/housing/distance_to_coast/<float:distance>')
+def get_housing_by_distance_to_coast(distance):
+    cur.execute('SELECT * FROM Housing WHERE Distance_to_coast <= ?', (distance,))
+    data = cur.fetchall()
+    return jsonify(data)
 
+@app.route('/api/housing/median_house_value/<int:value>')
+def get_housing_by_median_house_value(value):
+    cur.execute('SELECT * FROM Housing WHERE Median_House_Value = ?', (value,))
+    data = cur.fetchall()
+    return jsonify(data)
 
+@app.route('/api/housing/median_income/<int:value>')
+def get_housing_by_median_income(value):
+    cur.execute('SELECT * FROM Housing WHERE Median_Income = ?', (value,))
+    data = cur.fetchall()
+    return jsonify(data)
 
+@app.route('/api/housing/median_age/<int:value>')
+def get_housing_by_median_age(value):
+    cur.execute('SELECT * FROM Housing WHERE Median_Age = ?', (value,))
+    data = cur.fetchall()
+    return jsonify(data)
+
+@app.route('/api/housing/tot_rooms/<int:value>')
+def get_housing_by_tot_rooms(value):
+    cur.execute('SELECT * FROM Housing WHERE Tot_Rooms = ?', (value,))
+    data = cur.fetchall()
+    return jsonify(data)
+
+@app.route('/api/housing/tot_bedrooms/<int:value>')
+def get_housing_by_tot_bedrooms(value):
+    cur.execute('SELECT * FROM Housing WHERE Tot_Bedrooms = ?', (value,))
+    data = cur.fetchall()
+    return jsonify(data)
+
+@app.route('/api/housing/population/<int:value>')
+def get_housing_by_population(value):
+    cur.execute('SELECT * FROM Housing WHERE Population = ?', (value,))
+    data = cur.fetchall()
+    return jsonify(data)
 
 
 
