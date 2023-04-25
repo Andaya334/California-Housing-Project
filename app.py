da98117db03cb627baf6fceaa21e732daae5063f
Andaya334 <Andaya334@gmail.com>
2023-04-24 18:05:31 -0700

    new save

--- app.py
+++ app.py
@@ -25,67 +25,67 @@ def get_housing():
 
 @app.route('/api/housing/distance_to_la/<float:distance>')
 def get_housing_by_distance_to_la(distance):
-    cur.execute('SELECT * FROM Housing WHERE Distance_to_LA <= ?', (distance,))
+    cur.execute('SELECT * FROM Housing WHERE Distance_to_LA', (distance,))
     data = cur.fetchall()
     return jsonify(data)
 
 @app.route('/api/housing/distance_to_sf/<float:distance>')
 def get_housing_by_distance_to_sf(distance):
-    cur.execute('SELECT * FROM Housing WHERE Distance_to_SanFrancisco <= ?', (distance,))
+    cur.execute('SELECT * FROM Housing WHERE Distance_to_SanFrancisco', (distance,))
     data = cur.fetchall()
     return jsonify(data)
 
 @app.route('/api/housing/distance_to_sd/<float:distance>')
 def get_housing_by_distance_to_sd(distance):
-    cur.execute('SELECT * FROM Housing WHERE Distance_to_SanDiego <= ?', (distance,))
+    cur.execute('SELECT * FROM Housing WHERE Distance_to_SanDiego', (distance,))
     data = cur.fetchall()
     return jsonify(data)
 
 @app.route('/api/housing/distance_to_sj/<float:distance>')
 def get_housing_by_distance_to_sj(distance):
-    cur.execute('SELECT * FROM Housing WHERE Distance_to_SanJose <= ?', (distance,))
+    cur.execute('SELECT * FROM Housing WHERE Distance_to_SanJose', (distance,))
     data = cur.fetchall()
     return jsonify(data)
 
 @app.route('/api/housing/distance_to_coast/<float:distance>')
 def get_housing_by_distance_to_coast(distance):
-    cur.execute('SELECT * FROM Housing WHERE Distance_to_coast <= ?', (distance,))
+    cur.execute('SELECT * FROM Housing WHERE Distance_to_coast', (distance,))
     data = cur.fetchall()
     return jsonify(data)
 
 @app.route('/api/housing/median_house_value/<int:value>')
 def get_housing_by_median_house_value(value):
-    cur.execute('SELECT * FROM Housing WHERE Median_House_Value = ?', (value,))
+    cur.execute('SELECT * FROM Housing WHERE Median_House_Value', (value,))
     data = cur.fetchall()
     return jsonify(data)
 
 @app.route('/api/housing/median_income/<int:value>')
 def get_housing_by_median_income(value):
-    cur.execute('SELECT * FROM Housing WHERE Median_Income = ?', (value,))
+    cur.execute('SELECT * FROM Housing WHERE Median_Income', (value,))
     data = cur.fetchall()
     return jsonify(data)
 
 @app.route('/api/housing/median_age/<int:value>')
 def get_housing_by_median_age(value):
-    cur.execute('SELECT * FROM Housing WHERE Median_Age = ?', (value,))
+    cur.execute('SELECT * FROM Housing WHERE Median_Age', (value,))
     data = cur.fetchall()
     return jsonify(data)
 
 @app.route('/api/housing/tot_rooms/<int:value>')
 def get_housing_by_tot_rooms(value):
-    cur.execute('SELECT * FROM Housing WHERE Tot_Rooms = ?', (value,))
+    cur.execute('SELECT * FROM Housing WHERE Tot_Rooms', (value,))
     data = cur.fetchall()
     return jsonify(data)
 
 @app.route('/api/housing/tot_bedrooms/<int:value>')
 def get_housing_by_tot_bedrooms(value):
-    cur.execute('SELECT * FROM Housing WHERE Tot_Bedrooms = ?', (value,))
+    cur.execute('SELECT * FROM Housing WHERE Tot_Bedrooms', (value,))
     data = cur.fetchall()
     return jsonify(data)
 
 @app.route('/api/housing/population/<int:value>')
 def get_housing_by_population(value):
-    cur.execute('SELECT * FROM Housing WHERE Population = ?', (value,))
+    cur.execute('SELECT * FROM Housing WHERE Population', (value,))
     data = cur.fetchall()
     return jsonify(data)
 
